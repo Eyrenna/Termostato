00b8d620d3c3afa1ce2734f2f3e97bb3ca2e42e0
Claudia <claudia365x@gmail.com>
2019-04-09 00:03:21 +0200

    Creado nueva clase que implementa Calentable, version Game of Thrones,Daenerys

---
+++ src/main/java/got/Daenerys.java
@@ -0,0 +1,22 @@
+package got;
+
+import ambiente.Ambiente;
+import interfaces.Calentable;
+
+public class Daenerys implements Calentable {
+
+    //Atributos
+
+    private final int DRAGONFUEGO = 150419;
+    private final int DRAGONHIELO = 137117;
+
+    //Métodos
+
+    public void engage(Ambiente temperatura) {
+        temperatura.modificarTemperatura(this.DRAGONFUEGO);
+    }
+
+    public void disengage(Ambiente temperatura) {
+        temperatura.modificarTemperatura(this.DRAGONHIELO);
+    }
+}
