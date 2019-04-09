cd35403b9cd7eab6c2b9924f6ece0786e41fd167
Claudia <claudia365x@gmail.com>
2019-04-09 17:55:05 +0200

    Funciona todo ok; añadidas salidas de consola

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,6 +3,7 @@ package main;
 import ambiente.Ambiente;
 import componentes.calentador.Calentador;
 import componentes.calentador.EstufaSolar;
+import componentes.regulador.Normalizador;
 import componentes.regulador.Regulador;
 import componentes.Termometro;
 import got.Daenerys;
@@ -24,11 +25,15 @@ public class Main {
 
         Regulador regulador = new Regulador();
 
+        System.out.println("\n_____________________Radiador____________________\n");
+
         System.out.println( "Arrancando..." );
         regulador.regular(sensorTemp, radiador, minTemp, maxTemp, temperatura);
 
         /*_____SALIDA CONSOLA_____*/
         /*
+        _____________________Radiador____________________
+
         Arrancando...
         Calentando => temperatura 16.0
         Calentando => temperatura 17.0
@@ -44,31 +49,53 @@ public class Main {
         Apagado => temperatura 15.0
          */
 
+        System.out.println("\n_____________________Solaris____________________\n");
+
         Calentable solaris = new EstufaSolar();
         ((EstufaSolar) solaris).setReservas(60);
         ((EstufaSolar) solaris).setAlertaReserva(35);
+        Regulable regularis = new Normalizador();
         System.out.println( "Arrancando sistema de energía solar...");
-        regulador.regular(sensorTemp, solaris, minTemp, maxTemp, temperatura);
+        regularis.regular(sensorTemp, solaris, minTemp, maxTemp, temperatura);
         System.out.println( "Estado de la reserva de energía: " + ((EstufaSolar) solaris).getReservas());
 
-        /*___SALIDA CONSOLA____*/                       /*Calculos Reserva*/
-        /*
+        /*___SALIDA CONSOLA____*/
+        /*_____________________Solaris____________________
+
         Arrancando sistema de energía solar...
-        Calentando => temperatura 17.0                      >50
-        Calentando => temperatura 19.0                      >40
-        Calentando => temperatura 21.0                      >30
-        Apagado => temperatura 19.5                         >35
-        Apagado => temperatura 18.0                         >40
-        Apagado => temperatura 16.5                         >45
-        Apagado => temperatura 15.0                         >50   CORRECTO!!
+        Calentando => temperatura 17.0
+        Calentando => temperatura 19.0
+        Calentando => temperatura 21.0
+        Apagado => temperatura 19.5
+        Apagado => temperatura 18.0
+        Apagado => temperatura 16.5
+        Apagado => temperatura 15.0
         Estado de la reserva de energía: 50.0
         */
 
-        /*Calentable solarjet = new EstufaSolar();
+        System.out.println("\n_____________________Solarjet____________________\n");
+
+        Calentable solarjet = new EstufaSolar();
         ((EstufaSolar) solarjet).setReservas(20);
         ((EstufaSolar) solarjet).setAlertaReserva(15);
+        Regulable regularjet = new Normalizador();
         System.out.println( "Arrancando sistema de energía solar...");
-        regulador.regular(sensorTemp, solarjet, minTemp, maxTemp, temperatura);*/
+        regularjet.regular(sensorTemp, solarjet, minTemp, maxTemp, temperatura);
+        System.out.println( "Estado de la reserva de energía: " + ((EstufaSolar) solarjet).getReservas());
+
+        /*___SALIDA CONSOLA____*/
+        /*
+        _____________________Solarjet____________________
+
+        Arrancando sistema de energía solar...
+        Calentando => temperatura 17.0
+        Apagado => temperatura 15.5
+        Apagado => temperatura 14.0
+        Estado de la reserva de energía: 20.0
+        */
+
+
+        System.out.println("\n_____________________Daenerys____________________\n");
 
         Calentable dragones = new Daenerys();
         Regulable jonNieve = new GuardiaNoche();
@@ -77,5 +104,17 @@ public class Main {
                 "\nProtectora de los Siete Reinos, Madre de Dragones, la Khaleesi del Gran Mar de Hierba  " +
                 "\nla que no arde, Rompedora de cadenas");
         jonNieve.regular(sensorTemp, dragones, minTemp, maxTemp, temperatura);
+
+        /*
+        _____________________Daenerys____________________
+
+        Hace su entrada Daenerys de la Tormenta de la casa Targaryen,
+                legítima heredera del Trono de Hierro, Reina de los Ándalos y los Primeros Hombres,
+                Protectora de los Siete Reinos, Madre de Dragones, la Khaleesi del Gran Mar de Hierba
+        la que no arde, Rompedora de cadenas
+        DRACARYS...=> temperatura 150433.0
+        Winter is coming => temperatura 13316.0
+        Winter is coming => temperatura -123801.0
+        */
     }
 }
