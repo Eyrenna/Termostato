13600a20eed391785a211e1deeff7e21c3a2d87d
Claudia <claudia365x@gmail.com>
2019-04-09 17:31:45 +0200

    Añadida más teoría a singleton

--- src/main/java/ambiente/Ambiente.java
+++ src/main/java/ambiente/Ambiente.java
@@ -6,7 +6,11 @@ public class Ambiente {
     /*
     * -Instancia privada y estática
     * -Constructor privado
-    * -getObjeto para "new Objeto" public y static
+    * -getObjeto para "new Objeto" public y static(obligatorio porque la instancia es privada por lo que para acceder a un
+     * método de un objeto privado se necesita un punto de acceso goblal)
+    * -Recomendable poner la instancia del objeto en mayúsculas dando a entender que es constante
+    *
+    * *El singleton es del estilo del Enum en el que se crean un número determinado de instancias.
     */
 
     //Atributos
